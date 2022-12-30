fd23955ff5007dd337f986fc179050a6d5a84ffc
Daniel0402abera <37146428+Daniel0402abera@users.noreply.github.com>
2022-12-30 16:51:29 +0300

    peer added

--- public/script.js
+++ public/script.js
@@ -1,28 +1,62 @@
+const socket = io("/");
+const myPeer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3000",
+});
+
 const videoGrid = document.getElementById("video-grid");
-const myVideo= document.createElement('video');
+const myVideo = document.createElement("video");
 navigator.mediaDevices
   .getUserMedia({
-    video:true,
-    audio:true,
+    video: true,
+    audio: true,
   })
   .then((stream) => {
-    
     myVideoStream = stream;
+    addVideoStream(myVideo, stream);
 
-    addVideoStream(myVideo,stream);
+    myPeer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user_connected", (userid) => {
+      connectToNewUser(userid, stream);
+    });
 
   })
   .catch((err) => {
     console.log(err);
   });
 
+// socket.emit("join-room", roomid);
+// socket.on("user-connected", (userid) => {
+//   connectToNewUser(userid);
+// });
 
-  const addVideoStream = (video,stream) => {
-    video.srcObject = stream;
-    video.addEventListener('loadedmetadata' , () => {
-      video.play();
-    });
+myPeer.on("open", (id) => {
+  console.log(id);
+  socket.emit("join-room", roomid, id);
+});
 
-    videoGrid.append(video);
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
 
-  };
+  videoGrid.append(video);
+};
+
+const connectToNewUser = (userid, stream) => {
+  console.log("daz from front", userid);
+  const call = myPeer.call(userid, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
--- server.js
+++ server.js
@@ -1,19 +1,51 @@
 const express = require("express");
 
+//add express
 const app = express();
 
 const server = require("http").Server(app);
+//add io socket
+const io = require("socket.io")(server);
+//add uuid
 const { v4: uuidv4 } = require("uuid");
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
+// set view engine
+app.set("view engine", "ejs");
+// static routes
+app.use(express.static("public"));
+app.use("/peerjs", peerServer);
+
+
+// routes to uuid
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
 });
 
+// routes to id
 app.get("/:id", (req, res) => {
   res.render("room", { roomid: req.params.id });
 });
 
+
+
+// io connection
+io.on('connection', (socket) => {
+    socket.on('join-room',(roomid,userid)=>{
+        console.log('Daz Joined',roomid);
+        socket.join(roomid);
+        socket.to(roomid).emit('user-connected',userid);
+        
+    })
+
+})
+
+
+
+// server listen
 server.listen(process.env.PORT || 3000);
+
